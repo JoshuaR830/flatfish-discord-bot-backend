d04a8e60150da2f2b8a52e617636a56737987a14
Joshua <psyjr4@nottingham.ac.uk>
2019-12-15 13:00:34 +0000

    Requests now resolve

--- index.js
+++ index.js
@@ -8,22 +8,27 @@ var messageChannel;
 
 app.get("/check-pending", (req, res) => {
     fetch('http://www.flatfish.online:49162/check-pending');
+    res.status(200).send("Sent message to discord");
 })
 
 app.get("/set-pending", (req, res) => {
     fetch('http://www.flatfish.online:49162/set-pending');
+    res.status(200).send("Sent message to discord");
 })
 
 app.get("/deploy-pending", (req, res) => {
     fetch('http://www.flatfish.online:49162/deploy-pending');
+    res.status(200).send("Sent message to discord");
 })
 
 app.get("/confirm-pending", (req, res) => {
     fetch('http://www.flatfish.online:49162/confirm-pending');
+    res.status(200).send("Sent message to discord");
 })
 
 app.get("/", (req, res) => {
     fetch('http://www.flatfish.online:49162/');
+    res.status(200).send("Sent message to discord");
 })
 
 app.listen(8000, () => {
