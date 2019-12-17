0e7ad153472ec2bfb8d87e7a3b2f5374f5517a8a
Joshua <joshua.richardson@myunidays.com>
2019-12-17 12:52:30 +0000

    Addedd comments and reordered for clarity

--- index.js
+++ index.js
@@ -3,62 +3,86 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 
-const app = express()
-app.use(bodyParser());
-
+const app = express();
 
 var messageChannel;
+
+require('dotenv').config();
+
+app.use(bodyParser());
+
+app.listen(8000, () => {
+	console.log('Listening on port 8000!');
+});
 
 
 // Stubs for all of the functions that will actually do stuff and then return a status
 // Currently just returning a negative status
 // You would always hope that the actual responses would be the other way around - at least in the ideal case
 
+
+// Checks specified environment for pending version
+// Possibly just hit the url - is it up? Is it a 500 error?
+// If request fails, there is nothing there
+// Return whether or not there is something there - true - there is
 function checkPendingVersions(environment) {
     console.log("Check pending for " + environment);
     return true;
 }
 
-function isInDeployedEnvironment(branch) {
-    console.log("Has deployed " + branch + "successfully");
-    return false;
-}
-
-function isInTestEnvironment(branch) {
-    console.log("Has tested " + branch + "successfully");
-    return false;
-}
-
+// This will list all of the branches that are candidates for the specified environment
+// Only branches up to date with master can be deployment candidates
+// Any branch can be tested
 function getListOfBranches(environment) {
     console.log("list for " + environment);
     return ['master', 'develop'];
 }
 
+// This will do everything to setup the deployment candidate environment
+// Checkout correct project
+// Pull the latest code for the project
+// Create the new image
+// New up the docker container
+// Return true or false depending on whether it succeeded
+function isInDeployedEnvironment(branch) {
+    console.log("Has deployed " + branch + "successfully");
+    return false;
+}
+
+// This will do everything to setup the test candidate environment
+// Checkout correct project
+// Pull the latest code for the project
+// Create the new image
+// New up the docker container
+// Return true or false depending on whether it succeeded
+function isInTestEnvironment(branch) {
+    console.log("Has tested " + branch + "successfully");
+    return false;
+}
+
+// This will tear down the deployment candidate container
+// Need to stop current deployment container
+// Need to remove the docker container completely
+// Need to stop existing live container
+// Need to remove current live container - it restarts
+// Use the image from deployment to for the live container
+// Return whether or not this was done successfully as a Boolean
 function deployToLive(project, branch) {
     console.log("Deployed " + branch + " for " + project);
     return false;
 }
 
+// Environment specified must be torn down
+// Stop the container
+// Remove the container
+// Return whether or not this was done successfully as Boolean
 function rejectCandidate(environment) {
     console.log("Rejected " + environment + "environment");
     return false;
 }
 
-
-app.get("/check-pending", (req, res) => {
-    var isPending = checkPendingVersions();
-    request.post(
-        'http://www.flatfish.online:49162/check-pending',
-        { json: { hasPendingVersion: isPending, token: process.env.CONFIRM_TOKEN } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-            }
-        }
-    );
-    res.status(200).send("Post request: sent message to discord");
-})
-
+// This endpoint is hit when user asks for a list
+// Depending on the list depends on what is posted back
 app.post("/list", (req, res) => {
     let env = req.body.environment;
 
@@ -76,6 +100,9 @@ app.post("/list", (req, res) => {
     );
 })
 
+// User requests an environment
+// Performs the check
+// This posts back the envvironment checked and the status of that environment
 app.post("/check", (req, res) => {
     let env = req.body.environment;
 
@@ -92,6 +119,9 @@ app.post("/check", (req, res) => {
     );
 })
 
+// This calls the function to create a deployment candidate for the specified branch
+// Status of deployment sent back
+// TO-DO: Take the project name to deploy
 app.post("/deploy", (req, res) => {
     let branch = req.body.branch;
 
@@ -108,6 +138,9 @@ app.post("/deploy", (req, res) => {
     );
 })
 
+// This creates a test candidate for the specified branch
+// Status of test is sent back
+// TO-DO: Take the project name to test
 app.post("/test", (req, res) => {
     let branch = req.body.branch;
 
@@ -124,6 +157,9 @@ app.post("/test", (req, res) => {
     );
 })
 
+// This calls function to set the current deployment candidate to live
+// User specifies what branch and of which project in their message
+// Posts the status, project and the branch back to user
 app.post("/confirm-deployable", (req, res) => {
     let branch = req.body.branch;
     let project = req.body.project;
@@ -139,6 +175,10 @@ app.post("/confirm-deployable", (req, res) => {
     );
 })
 
+// User can reject either environment
+// Specify in their message
+// Calls function to take that environment down
+// Posts back the status for the bot to deal with
 app.post("/reject", (req, res) => {
     let environment = req.body.environment;
 
@@ -155,30 +195,3 @@ app.post("/reject", (req, res) => {
         }
     );
 })
-
-// app.get("/set-pending", (req, res) => {
-//     fetch('http://www.flatfish.online:49162/set-pending');
-//     res.status(200).send("Sent message to discord");
-// })
-
-// app.get("/deploy-pending", (req, res) => {
-//     fetch('http://www.flatfish.online:49162/deploy-pending');
-//     res.status(200).send("Sent message to discord");
-// })
-
-// app.get("/confirm-pending", (req, res) => {
-//     fetch('http://www.flatfish.online:49162/confirm-pending');
-//     res.status(200).send("Sent message to discord");
-// })
-
-// app.get("/", (req, res) => {
-//     fetch('http://www.flatfish.online:49162/');
-//     res.status(200).send("Sent message to discord");
-// })
-
-app.listen(8000, () => {
-	console.log('Listening on port 8000!');
-});
-
-
-require('dotenv').config();
